29646eb7494d2758aa52c772c3c4c77c002607a4
acexyf <corner_scofield@163.com>
2018-10-10 23:05:46 +0800

    优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var babel = require('gulp-babel');
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
+gulp.task('build-pages',function(cb){
+
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('build/'));
+
+})
+
+
+
+gulp.task('build-less', function () {
+    return gulp.src('./src/css/index.less')
+      .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('build-js', function(){
+    return gulp.src('./src/js/index.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest('build/js'))
+})
+
+
+gulp.task('default', ['build-js','build-less','build-pages'],function() {
+    console.log('done')
 });
