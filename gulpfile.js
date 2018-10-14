d39dcafc38e4ac8694d9b1d6aa0fabe517f579b9
acexyf <corner_scofield@163.com>
2018-10-15 00:22:20 +0800

    页面注入

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var babel = require('gulp-babel');
+var uglify = require("gulp-uglify");
+var csso = require('gulp-csso');
+var fileinclude = require('gulp-file-include');
+
 
 gulp.task('build-pages',function(cb){
 
     return gulp.src('./src/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
         .pipe(gulp.dest('build/'));
 
 })
@@ -17,6 +25,11 @@ gulp.task('build-less', function () {
       .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
       }))
+    //   pipe(csso({
+    //     restructure: false,
+    //     sourceMap: true,
+    //     debug: true
+    //     }))
       .pipe(gulp.dest('build/css/'));
 });
 
@@ -25,10 +38,11 @@ gulp.task('build-js', function(){
     .pipe(babel({
         presets: ['es2015']
     }))
+    // .pipe(uglify())
     .pipe(gulp.dest('build/js'))
 })
 
 
 gulp.task('default', ['build-js','build-less','build-pages'],function() {
-    console.log('done')
+    console.log('done');
 });
